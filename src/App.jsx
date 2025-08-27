f8eacbc156e91d6a7c826bc09886c8820aad3041
Your Name <you@example.com>
2025-08-27 22:55:42 +0100

    feat:  cambiado sistema de como se cuenta el tiempo en el timer y ajustado a que despues de serie de 25 minutos vaya a uno 5 minutos

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,66 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [timer, setTimer] = useState({ time: new Date(0,0,0) });
+  const [timer, setTimer] = useState({ time:{ seconds: 20, minutes: 0, hours: 0 } });
+  const [started, setStarted] = useState(false);
+  const [numberPomodoro, setNumberPomodoro] = useState({
+    pomodoro25Min: 0,
+    pomodoro5Min: 0,
+    pomodoro15Min: 0,
+  });
+  const [actualPomodoro, setActualPomodoro] = useState('25min');
   useEffect(() => {
+        console.log(actualPomodoro)
+        console.log(numberPomodoro)
+        console.log(timer)
+        if(!started) return;
     const interval = setInterval(() => {
+      if(timer.time.seconds === 0 && timer.time.minutes === 0){
+        setActualPomodoro(prevPomodoro => {
+          if(prevPomodoro === '25min'){
+            setStarted(false);
+            setTimer({ time: { seconds: 0, minutes: 5, hours: 0 } });
+            return '5min';
+          }
+          if(prevPomodoro === '5min'){
+            setStarted(false);
+            setTimer({ time: { seconds: 0, minutes: 15, hours: 0 } });
+            return '15min';
+          }
+          setStarted(false);
+          setTimer({ time: { seconds: 0, minutes: 25, hours: 0 } });
+          return '25min';
+        });
+      }
+      console.log(actualPomodoro)
       setTimer(prevTimer => {
-        const newTime = new Date(prevTimer.time || Date.now() + 1000);
-        newTime.setSeconds(newTime.getSeconds() + 1);
-        return { time: newTime };
+        if(prevTimer.time.seconds <1){
+          return { time: { seconds: 59, minutes: prevTimer.time.minutes - 1, hours: prevTimer.time.hours } }
+        }
+          return { time: { seconds: Number(prevTimer.time.seconds) - 1, minutes: prevTimer.time.minutes, hours: prevTimer.time.hours } }
+
+       
       });
+
+      
     }, 1000);
     return () => clearInterval(interval);
-  }, []);
+  }, [started,numberPomodoro,actualPomodoro,timer]);
   return (
     <>
       <div className='MainContainer'>
         <div className="container">
           <h2>Timer</h2>
-
+        
           <div className="Timmer">
+            
             <div className="Timmer__display">
-              <span className="Timmer__display--number">{new Date(timer.time).toLocaleTimeString()}</span>
+               <div className={`spinner ${started ? '' : 'hidden'}`}></div>
+              <span className="Timmer__display--number">{`${timer.time.minutes}:${timer.time.seconds}`}</span>
             </div>
+            <button onClick={() => setStarted(!started)}>
+              {started ? 'Stop' : 'Start'}
+            </button>
           </div>
         </div>
       </div>
